ac9bedae76b2cf402983879890a6ce95c48eedfe
Upendra1008 <upendrakumar236@gmail.com>
2026-07-02 20:31:32 +0530

    Update main.tf

--- Ec2 Creation/main.tf
+++ Ec2 Creation/main.tf
@@ -1,1 +1,12 @@
+resource "aws_instance" "webserver" {
 
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name        = "Terraform-EC2"
+    Environment = "Dev"
+    Owner       = "Debdip"
+  }
+}
