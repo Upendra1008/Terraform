27a205c3c16f46317d88858643e26fca3e598f6b
Upendra1008 <upendrakumar236@gmail.com>
2026-07-02 20:34:32 +0530

    Update variables.tf

--- Ec2 Creation/variables.tf
+++ Ec2 Creation/variables.tf
@@ -1,1 +1,39 @@
+variable "region" {
 
+  description = "eu-central-1"
+
+  type = string
+
+  default = "eu-central-1"
+
+}
+
+
+
+variable "ami" {
+
+  description = "ami-0c541e8575db12991"
+
+  type = string
+
+}
+
+
+
+variable "instance_type" {
+
+  description = "t3.micro"
+
+  type = string
+
+}
+
+
+
+variable "key_name" {
+
+  description = "debops_11"
+
+  type = string
+
+}
